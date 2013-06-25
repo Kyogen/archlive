247e2d37bef0b0b39cd85b7713c5ca2a166f7633
Thomas Triller <twtriller@gmail.com>
2013-06-25 13:48:13 +0200

    user "arch" zu Gruppe wireshark hinzugefügt

--- root-image/root/customize_root_image.sh
+++ root-image/root/customize_root_image.sh
@@ -10,7 +10,7 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 usermod -s /bin/bash root
 cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,video,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash arch
+useradd -m -p "" -g users -G "adm,audio,video,floppy,log,network,rfkill,scanner,storage,optical,power,wheel,wireshark" -s /bin/bash arch
 
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
